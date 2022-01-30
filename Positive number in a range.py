dd75e2622cb7f30784c68df7f97786ee6c5840d9
Sowmya1729 <97968654+Sowmya1729@users.noreply.github.com>
2022-01-30 19:38:55 +0530

    Add files via upload

---
+++ Positive number in a range.py
@@ -0,0 +1,12 @@
+for i in [12,-7,5,64,-14] :
+            if i >0 :
+            	print(i)
+for j in [12,14,-95,3] :
+              if j>0 :
+                 print([j])
+                          	
+                          	
+                          	
+                          	
+             	
+	
